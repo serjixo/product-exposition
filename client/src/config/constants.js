1243ac5e8e87740dae5efffb3d6330df5d0e5e23
Sergio Jimenez <serjicho@hotmail.com>
2023-04-20 17:02:31 +0200

    modification of git ignore and deletion of files

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,4 @@
-import { swatch, fileIcon, ai, logoShirt, stylishShirt } from "../assets";
+import { swatch, fileIcon, ai, logoShirt, stylishShirt } from "../assets/index.js";
 
 export const EditorTabs = [
   {
