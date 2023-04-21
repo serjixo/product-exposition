e03fd92f1aba37d1985d0ba546f7c1a569b1b9dc
Sergio Jimenez <serjicho@hotmail.com>
2023-04-21 12:55:12 +0200

    adds engine node version for deployment tool render

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ import dalleRoutes from "./routes/dalle.routes.js";
 dotenv.config()
 
 const app = express()
-app.use(cors)
+app.use(cors())
 app.use(express.json({limit: '50mb'}))
-app.use('/api/v1/dalle',dalleRoutes)
+app.use('/api/v1/dalle', dalleRoutes)
 app.get('/', (req, res) => {
     res.status(200).json({message: 'hello from dall.e'})
 })
 
-app.listen(8080,()=> console.log('Server has started on port 8080'))
+app.listen(8080, () => console.log('Server has started on port 8080'))
